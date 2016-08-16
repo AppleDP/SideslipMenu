2a1d7397d8cc5cfd7fd25527c4dcb0f6b6b6054e
AppleDP <AppleDP@163.com>
2016-08-16 16:13:52 +0800

    初始化项目

---
+++ SideslipMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SideslipMenu
+//
+//  Created by admin on 16/8/16.
+//  Copyright © 2016年 SUWQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SideslipMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SideslipMenu
+//
+//  Created by admin on 16/8/16.
+//  Copyright © 2016年 SUWQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
